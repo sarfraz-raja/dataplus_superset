fe9fbb13d445d20d741c2b41816c2d13675bf218
Vinay Gupta <vinay.gupta@datayog.com>
2026-02-02 14:31:55 +0530

    server code

--- superset_config.py
+++ superset_config.py
@@ -2,17 +2,22 @@
 # PostgreSQL as Metadata DB
 # ===============================
 
-# SQLALCHEMY_DATABASE_URI = (
-#     "postgresql+psycopg2://superset:superset123@localhost:5432/superset"
-# )
 SQLALCHEMY_DATABASE_URI = (
     "postgresql+psycopg2://vinay:vinay123@192.168.0.100:5432/superset"
 )
-SECRET_KEY = "change-this-to-long-random-secret"
+
+SECRET_KEY = "971798e5e6d5ede611a235cda7f72ae71c24bb08bf40219a4fd1d631e69b8a11"
 
 ROW_LIMIT = 10000
 SUPERSET_WEBSERVER_TIMEOUT = 300
 
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "pool_pre_ping": True,
+    "pool_recycle": 1800,
+}
+
 FEATURE_FLAGS = {
     "DASHBOARD_NATIVE_FILTERS": True,
+    "ENABLE_TEMPLATE_PROCESSING": True,
+    "SQLLAB_BACKEND_PERSISTENCE": True,
 }
